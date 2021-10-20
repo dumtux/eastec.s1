7d5d3bf637a90f51087e785a471e7cfc09a18348
Hotte Shen <hotteshen@gmail.com>
2021-10-20 19:29:27 +0800

    add update logic to KFive, untested.

--- software/sone/kfive.py
+++ software/sone/kfive.py
@@ -7,18 +7,20 @@ from .models import Status
 class KFive(Singleton):
     HEATER_VALS = {0: 0, 1: 30, 2: 50, 3: 70, 4: 100}
 
-    heater: bool  = False  # on/off, determined by set temperature vs read temperature, set time vs ellapsed time
+    heater: bool  = False  # on/off, determined by target temperature vs read temperature, target time vs ellapsed time
     pwr: bool  = False     # on/off
     lac: bool  = False     # on/off
     l1_mode: int  = 0      # 0 - 8
     l2: bool  = False      # on/off
-    temp: int  = 0         # temperature 20 - 70
+    target_temperature: int  = 0         # temperature 20 - 70
+    read_temperature: int  = 0           # read temperature from box
     time: int  = 0         # minutes 0 - 90
     auto_hh: int  = 0      # automode hour 0 - 23
     auto_mm: int  = 0      # automode minute 0 - 59
     ht1: int  = 0          # heater 1 level 0 - 4, 0-0%, 1-30%, 2-50%, 3-75$, 4-100%
     ht2: int  = 0          # heater 2 level 0 - 4
     ht3: int  = 0          # heater 3 level 0 - 4
+    endbyte: int = 0x42    # changes
 
     def to_bytes(self) -> bytes:
         a = []
@@ -27,8 +29,8 @@ class KFive(Singleton):
         a.append(self.time)
         a.append(self.auto_hh)
         a.append(self.auto_mm)
-        a.append(self.temp)
-        a.append(int(self.temp * 9 / 5) + 32)
+        a.append(self.target_temperature)
+        a.append(int(self.target_temperature * 9 / 5) + 32)
         a.append(0)
         a.append(0)
         a.append(0)
@@ -36,7 +38,7 @@ class KFive(Singleton):
         a.append(self.HEATER_VALS[self.ht2])
         a.append(self.HEATER_VALS[self.ht3])
         a.append(self.l1_mode)
-        a.append(0x42)
+        a.append(self.endbyte)
         a.append((sum(a) - 0xcc - 138) % 256)
         t = ' '.join([format(x, '02x') for x in a])
         print(t)
@@ -44,13 +46,32 @@ class KFive(Singleton):
         return bytes(ba)
 
     def update(status: Status) -> Status:
-        self.temp = status.target_temperature
+        self.target_temperature = status.target_temperature
         self.time = status.target_timer
+        self.ht1 = status.heaters[0].level
+        self.ht2 = status.heaters[1].level
+        self.ht3 = status.heaters[2].level
 
-    def write(self):
-        # data = self.to_bytes()
-        # s.write(data)
-        raise Exception("Not implemented yet")
+        if self.time == 0:
+            self.pwr = False
+            self.heater = False
+            self.endbyte = 0x42
+            status.state = 'standby'
+        else:
+            if status.state == 'standby':
+                self.pwr = False
+                self.heater = False
+                self.endbyte = 0x42
+            elif status.state == 'playing':
+                self.pwr = True
+                self.heater = self.target_temperature > self.read_temperature
+                self.endbyte = 0xC2 if self.heater else 0x02
+            elif status.state == 'paused':
+                self.pwr = False
+                self.heater = False
+                self.endbyte = 0x42
 
-    def read(self, callback: Callable):
-        raise Exception("Not implemented yet")
+        # self.uart.write(self.to_bytes())
+        print(self.to_bytes())
+
+        return status
