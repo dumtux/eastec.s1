1faf6d915cf9c0d8afe07917f107903acee955c6
Hotte Shen <hotteshen@gmail.com>
2021-10-24 22:22:10 +0800

    refactoring.

--- software/sone/api.py
+++ software/sone/api.py
@@ -16,7 +16,7 @@ app = FastAPI(
     version=__version__,
     description="Cloud REST API for sauna status fetching and control")
 connections: Dict[str, WebSocket] = dict()
-response_status: Dict[str, Any] = dict()
+responses: Dict[str, Any] = dict()
 
 
 @app.websocket_route("/ws/{sauna_id}", name="ws")
@@ -28,18 +28,18 @@ class DeviceCoManager(WebSocketEndpoint):
         await ws.accept()
         sauna_id = DeviceCoManager.get_id_from_ws(ws)
         connections[sauna_id] = ws
-        response_status[sauna_id] = None
+        responses[sauna_id] = None
         print(f"Sauna {sauna_id} connected.")
 
     async def on_disconnect(self, ws, close_code: int):
         sauna_id = DeviceCoManager.get_id_from_ws(ws)
         connections.pop(sauna_id)
-        response_status.pop(sauna_id)
+        responses.pop(sauna_id)
         print(f"Sauna {sauna_id} disconnected.")
 
     async def on_receive(self, ws, msg: Any):
         sauna_id = DeviceCoManager.get_id_from_ws(ws)
-        response_status[sauna_id] = json.loads(msg)
+        responses[sauna_id] = json.loads(msg)
 
     @classmethod
     def get_id_from_ws(cls, ws: WebSocket) -> str:
@@ -54,11 +54,7 @@ root_router = APIRouter(prefix="/sauna")
 meta_router = APIRouter(tags=["Sauna Meta"])
 status_router = APIRouter(
     tags=["Sauna Status"],
-    response_status={404: {"description": "Sauna ID not found", "model": HTTPError}})
-control_router = APIRouter(
-    tags=["Sauna Control"],
-    response_status={404: {"description": "Sauna ID not found", "model": HTTPError}})
-scheduling_router = APIRouter(tags=["Sauna Scheduling"])
+    responses={404: {"description": "Sauna ID not found", "model": HTTPError}})
 
 
 @meta_router.get("/list", response_model=List[str])
@@ -71,72 +67,14 @@ async def get_status(sauna_id: str):
     if sauna_id not in connections:
         raise HTTPException(status_code=404, detail="Sauna ID not found")
     await connections[sauna_id].send_json({"message": "is this working?"})
-    while response_status[sauna_id] is None:
+    while responses[sauna_id] is None:
         await asyncio.sleep(0.8)
-    r = dict(response_status[sauna_id])
-    response_status[sauna_id] = None
+    r = dict(responses[sauna_id])
+    responses[sauna_id] = None
     return r
-
-
-@control_router.put("/{sauna_id}/state", response_model=Status)
-async def update_state(sauna_id: str, update: StateUpdate):
-    raise Exception("Not implemented yet")
-
-
-@control_router.put("/{sauna_id}/temperature", response_model=Status)
-async def update_target_temperature(sauna_id: str, update: TemperatureUpdate):
-    raise Exception("Not implemented yet")
-
-
-@control_router.put("/{sauna_id}/timer", response_model=Status)
-async def update_timer(sauna_id: str, update: TimerUpdate):
-    raise Exception("Not implemented yet")
-
-
-@control_router.post("/{sauna_id}/program", response_model=Status)
-async def update_timer(sauna_id: str, program: Program):
-    raise Exception("Not implemented yet")
-
-
-@scheduling_router.get(
-    "/{sauna_id}/schedules",
-    response_model=List[Schedule],
-    response_status={
-        404: {"description": "Sauna ID not found", "model": HTTPError},
-    },
-)
-async def get_schedules(sauna_id: str):
-    raise Exception("Not implemented yet")
-
-
-@scheduling_router.post(
-    "/{sauna_id}/schedules",
-    status_code=201,
-    response_model=List[Schedule],
-    response_status={
-        404: {"description": "Sauna ID or Schedule ID not found", "model": HTTPError},
-        409: {"description": "Schedule ID conflicts", "model": HTTPError},
-    },
-)
-async def add_schedule(sauna_id: str, schedule: Schedule):
-    raise Exception("Not implemented yet")
-
-
-@scheduling_router.delete(
-    "/{sauna_id}/schedules/{schedule_id}",
-    response_model=List[Schedule],
-    response_status={
-        404: {"description": "Sauna ID or Schedule ID not found", "model": HTTPError},
-    },
-)
-async def delete_schedule(sauna_id: str, schedule_id: str):
-    raise Exception("Not implemented yet")
-
 
 root_router.include_router(meta_router)
 root_router.include_router(status_router)
-root_router.include_router(control_router)
-root_router.include_router(scheduling_router)
 app.include_router(root_router)
 
 app.add_middleware(
