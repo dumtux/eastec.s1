c256d837162ba54f0189385ca41d0d44769f4b1f
Hotte Shen <hotteshen@gmail.com>
2021-12-14 10:05:38 +0800

    fix.

--- sone/utils.py
+++ sone/utils.py
@@ -91,7 +91,7 @@ def reboot_os():
 def upgrade_firmware():
     with open(TOKEN_FILE_PATH) as f:
         token = f.read()
-    os.system("pip3 install --upgrade git+https://{token}@github.com/hotteshen/eastec.s1@release/1.0")
+    os.system(f"pip3 install --upgrade git+https://{token}@github.com/hotteshen/eastec.s1@release/1.0")
 
 
 def async_wrap(func):
