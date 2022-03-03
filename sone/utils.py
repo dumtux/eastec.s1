e35d133e432bdbaa7ed458fd3f10064dd0fddcbd
Kaj Suaning <ksuaning@gmail.com>
2022-03-03 13:03:14 +1100

    reverted firmware update method

--- sone/utils.py
+++ sone/utils.py
@@ -101,8 +101,8 @@ def upgrade_firmware():
     # Using SSH deploy token
     #NOTE: This requires ssh-add to be running and the key to be added.
     #os.system(f"pip3 install --no-cache-dir -U git+ssh://git@github.com/ksuaning-au/eastec.s1.git@release/1.0")
-    #os.system(f"pip3 install --no-cache-dir --upgrade git+https://gitlab.com/eastec/sone.git@release/1.0")
-    os.system('/bin/bash -c "source /home/pi/update.sh"')
+    os.system(f"pip3 install --no-cache-dir --upgrade git+https://gitlab.com/eastec/sone.git@release/1.0")
+    #os.system('/bin/bash -c "source /home/pi/update.sh"')
 
 
 def async_wrap(func):
